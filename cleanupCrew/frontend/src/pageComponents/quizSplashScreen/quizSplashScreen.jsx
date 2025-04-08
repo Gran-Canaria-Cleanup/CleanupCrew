38cba1563df2fc56228aefc13cb49faee1002073
MonikaKlaudia <monikaklaudiawie@gmail.com>
2025-04-08 13:55:23 +0100

    feat(frontend): create splash screen

--- cleanupCrew/frontend/src/pageComponents/quizSplashScreen/quizSplashScreen.jsx
+++ cleanupCrew/frontend/src/pageComponents/quizSplashScreen/quizSplashScreen.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './quizSplashScreen.scss';
+import { StartLogo }  from '../../components/startLogo/startLogo.jsx'
+import { QuizGetStarted } from '../../components/quizGetStarted/quizGetStarted.jsx';
 
 
 export const QuizSplashScreen = () => {
   return (
     <section className='quizSplashBody'>
-      <p>hello</p>
+      <StartLogo/>
+      <QuizGetStarted/>
     </section>
   );
 };
