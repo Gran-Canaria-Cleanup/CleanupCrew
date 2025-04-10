5588b2aeeb412c2515219d50e0697d07b898a485
MikkelTMS <mtamsgaard@gmail.com>
2025-04-10 12:42:43 +0100

    feat(frontend): made the map page

--- cleanupCrew/frontend/src/pageComponents/map/mapScreen.jsx
+++ cleanupCrew/frontend/src/pageComponents/map/mapScreen.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './mapScreen.scss';
 import { Navbar } from '../../components/navbar/navbar.jsx';
+import { MapSection } from '../../components/map/map.jsx';
 
 export const MapScreen = () => {
   
   return (
     <section className='mapBody'>
+      <MapSection/>
       <Navbar/>
     </section>
   );
